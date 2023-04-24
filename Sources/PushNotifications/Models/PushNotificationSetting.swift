dc511c7e15c71a52cc42929ad57f2b22d94845b0
Sven Andabaka <sven.andabaka@tum.de>
2023-04-24 16:37:02 +0200

    make id optional

--- Sources/PushNotifications/Models/PushNotificationSetting.swift
+++ Sources/PushNotifications/Models/PushNotificationSetting.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public struct PushNotificationSetting: Codable {
-    public let id: Int64
+    public let id: Int64?
     public let settingId: PushNotificationSettingId
     public let webapp: Bool
     public let email: Bool
