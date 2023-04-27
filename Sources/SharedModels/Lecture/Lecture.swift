b28f67e138044787cef7532c7d4b187597a6dd99
Sven Andabaka <sven.andabaka@tum.de>
2023-04-27 19:00:58 +0200

    add lecture image and make lecture hashable and identifiable

--- Sources/SharedModels/Lecture/Lecture.swift
+++ Sources/SharedModels/Lecture/Lecture.swift
@@ -6,12 +6,19 @@
 //
 
 import Foundation
+import SwiftUI
 
-public struct Lecture: Codable {
+public struct Lecture: Codable, Identifiable {
     public let id: Int
     public let title: String?
     public let description: String?
     public let startDate: Date?
     public let endDate: Date?
     //    public let attachments: [Attachment]
+
+    public var image: Image {
+        return Image("chalkboard-teacher-solid", bundle: .module)
+    }
 }
+
+extension Lecture: Equatable, Hashable { }
