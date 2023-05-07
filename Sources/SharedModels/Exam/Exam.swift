7425e6af0b52cfe844631dbc41c78496091e0605
Tarlan Ismayilsoy <terlan_ismayilsoy@hotmail.com>
2023-05-07 13:16:12 +0400

    Change Exam date types from String? to Date?;

--- Sources/SharedModels/Exam/Exam.swift
+++ Sources/SharedModels/Exam/Exam.swift
@@ -10,10 +10,10 @@ public struct Exam: Codable {
     public let id: Int
     public let title: String
     /// Start of working time
-    public var startDate: String?
+    public var startDate: Date?
     /// End of working time, start of assessment time
-    public var endDate: String?
+    public var endDate: Date?
     /// End of assessment date
-    public var publishResultsDate: String?
+    public var publishResultsDate: Date?
     public var exerciseGroups: [ExerciseGroup]?
 }
