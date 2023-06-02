717d1cfa7d35065a163daa83835d83f88f20e1e9
Tarlan Ismayilsoy <terlan_ismayilsoy@hotmail.com>
2023-06-02 19:35:41 +0400

    Add TextBlocks

--- Sources/SharedModels/Exercise/Submission/TextSubmission.swift
+++ Sources/SharedModels/Exercise/Submission/TextSubmission.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CryptoKit
 
 public struct TextSubmission: BaseSubmission {
     public static var type: String {
@@ -21,4 +22,30 @@ public struct TextSubmission: BaseSubmission {
     public var participation: Participation?
 
     public var text: String?
+    public var blocks: [TextBlock]?
+}
+
+public struct TextBlock: Codable {
+    public var id: String?
+    public var text: String?
+    public var startIndex: Int?
+    public var endIndex: Int?
+    public var submission: TextSubmission?
+    public var type: TextBlockType
+    public var numberOfAffectedSubmissions: Int?
+
+    /// Computes and sets the id of this block (identical to de.tum.in.www1.artemis.domain.TextBlock:computeId)
+    public mutating func computeId() {
+        let submissionId = submission?.id ?? 0
+        let startIndex = startIndex ?? 0
+        let endIndex = endIndex ?? 0
+        let text = text ?? ""
+
+        let idData = "\(submissionId);\(startIndex)-\(endIndex);\(text)".data(using: .utf8) ?? Data()
+        self.id = Insecure.SHA1.hash(data: idData).map({ String(format: "%02hhx", $0) }).joined()
+    }
+}
+
+public enum TextBlockType: String, Codable {
+    case AUTOMATIC, MANUAL
 }
