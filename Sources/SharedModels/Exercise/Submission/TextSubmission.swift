af43c554d6680c40333ee6b4373924067d40ad86
Tarlan Ismayilsoy <terlan_ismayilsoy@hotmail.com>
2023-06-07 11:54:34 +0400

    Add init to TextBlock; Replace submission with submissionId

--- Sources/SharedModels/Exercise/Submission/TextSubmission.swift
+++ Sources/SharedModels/Exercise/Submission/TextSubmission.swift
@@ -30,13 +30,25 @@ public struct TextBlock: Codable {
     public var text: String?
     public var startIndex: Int?
     public var endIndex: Int?
-    public var submission: TextSubmission?
     public var type: TextBlockType
     public var numberOfAffectedSubmissions: Int?
+    
+    // not decoded/encoded
+    public var submissionId: Int?
+
+    public init(submissionId: Int?, text: String? = nil, startIndex: Int? = nil, endIndex: Int? = nil) {
+        self.id = nil
+        self.text = text
+        self.startIndex = startIndex
+        self.endIndex = endIndex
+        self.submissionId = submissionId
+        self.type = .MANUAL
+        self.numberOfAffectedSubmissions = nil
+    }
 
     /// Computes and sets the id of this block (identical to de.tum.in.www1.artemis.domain.TextBlock:computeId)
     public mutating func computeId() {
-        let submissionId = submission?.id ?? 0
+        let submissionId = submissionId ?? 0
         let startIndex = startIndex ?? 0
         let endIndex = endIndex ?? 0
         let text = text ?? ""
