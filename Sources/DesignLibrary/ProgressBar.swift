07850c9ab2ab37962f92bd16b8c38d76a0c6249c
Nityananda Zbil <nityananda.zbil@tum.de>
2024-03-18 01:43:34 +0100

    Color progress gray if 0

--- Sources/DesignLibrary/ProgressBar.swift
+++ Sources/DesignLibrary/ProgressBar.swift
@@ -8,16 +8,17 @@
 import SwiftUI
 
 public struct ProgressBar: View {
-
     let value: Int
     let total: Int
     let backgroundColor: Color
     let ringColor: Color
 
-    public init(value: Int,
-                total: Int,
-                backgroundColor: Color = Color.Artemis.courseScoreProgressBackgroundColor,
-                ringColor: Color = Color.Artemis.courseScoreProgressRingColor) {
+    public init(
+        value: Int,
+        total: Int,
+        backgroundColor: Color = Color.Artemis.courseScoreProgressBackgroundColor,
+        ringColor: Color = Color.Artemis.courseScoreProgressRingColor
+    ) {
         self.value = value
         self.total = total
         self.backgroundColor = backgroundColor
@@ -33,13 +34,13 @@ public struct ProgressBar: View {
             Circle()
                 .stroke(lineWidth: 20.0)
                 .foregroundColor(backgroundColor)
-
             Circle()
                 .trim(from: 0.0, to: CGFloat(min(self.progress, 1.0)))
                 .stroke(style: StrokeStyle(lineWidth: 20.0, lineCap: .round, lineJoin: .round))
-                .foregroundColor(ringColor)
+                .foregroundColor(total == 0 ? .gray : ringColor)
                 .rotationEffect(Angle(degrees: 270.0))
                 .animation(.linear, value: progress)
+
             VStack {
                 Text("\(value) / \(total)")
                     .font(.title3)
