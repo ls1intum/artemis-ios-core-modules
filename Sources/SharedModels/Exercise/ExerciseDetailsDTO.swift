9380f575e31cb0634c11d60fa5fdc14fb0e33b74
Anian Schleyer <98647423+anian03@users.noreply.github.com>
2024-07-07 18:17:50 +0200

    `Exercises`: Fix decoding failure for Exercise Details (#66)
    * Add ExerciseDetailsDTO

---
+++ Sources/SharedModels/Exercise/ExerciseDetailsDTO.swift
@@ -0,0 +1,12 @@
+//
+//  ExerciseDetailsDTO.swift
+//
+//
+//  Created by Anian Schleyer on 06.07.24.
+//
+
+import Foundation
+
+public struct ExerciseDetailsDTO: Codable {
+    public let exercise: Exercise
+}
