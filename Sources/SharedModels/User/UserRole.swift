c7c3199663717c5a5c9379b3df836f81da37e365
Anian Schleyer <anian.schleyer@tum.de>
2024-07-11 22:08:41 +0200

    Add displayName and badgeColor for UserRole

--- Sources/SharedModels/User/UserRole.swift
+++ Sources/SharedModels/User/UserRole.swift
@@ -6,9 +6,25 @@
 //
 
 import Foundation
+import SwiftUI
 
 public enum UserRole: String, RawRepresentable, Codable {
     case instructor = "INSTRUCTOR"
     case tutor = "TUTOR"
     case user = "USER"
+
+    public var displayName: String {
+        rawValue.capitalized
+    }
+
+    public var badgeColor: Color {
+        switch self {
+        case .instructor:
+            return .init(red: 204, green: 0, blue: 0)
+        case .tutor:
+            return .init(red: 253, green: 126, blue: 20)
+        case .user:
+            return .init(red: 23, green: 162, blue: 184)
+        }
+    }
 }
