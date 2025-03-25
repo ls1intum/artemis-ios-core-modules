8edb85803a58039a2c2e2e286150eccfd6eef594
Anian Schleyer <98647423+anian03@users.noreply.github.com>
2025-03-25 17:28:43 +0100

    Change TUM Artemis url

--- Sources/UserStore/Config.swift
+++ Sources/UserStore/Config.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum Config {
-    static let tumBaseEndpointUrl = URL(string: "https://artemis.ase.in.tum.de/")
+    static let tumBaseEndpointUrl = URL(string: "https://artemis.tum.de/")
     static let kitBaseEndpointUrl = URL(string: "https://artemis.praktomat.cs.kit.edu/")
     static let hmBaseEndpointUrl = URL(string: "https://artemis.cs.hm.edu/")
     static let codeAbilityBaseEndpointUrl = URL(string: "https://artemis.codeability.uibk.ac.at/")
