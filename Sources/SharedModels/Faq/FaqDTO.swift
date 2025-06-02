506951a894512454651f8c47dceec27b2e14a5a5
Anian Schleyer <anian.schleyer@tum.de>
2025-06-02 11:23:32 +0200

    Add categories to DTO

--- Sources/SharedModels/Faq/FaqDTO.swift
+++ Sources/SharedModels/Faq/FaqDTO.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 public struct FaqDTO: Codable, Identifiable {
     public var id: Int64?
@@ -14,4 +15,34 @@ public struct FaqDTO: Codable, Identifiable {
     public var categories: Set<String>?
     public var faqState: FaqState
     public var course: Course?
+
+    public var categoriesAsModel: [FaqCategory]? {
+        categories?.compactMap(FaqCategory.init(jsonString:))
+    }
+
+    public init() {
+        questionTitle = ""
+        questionAnswer = ""
+        categories = []
+        faqState = .unknown
+    }
+}
+
+public struct FaqCategory: Codable {
+    public let color: String
+    public let category: String
+
+    public var uiColor: UIColor {
+        UIColor(hexString: color)
+    }
+}
+
+extension FaqCategory {
+    init?(jsonString: String) {
+        if let decoded = try? JSONDecoder().decode(Self.self, from: Data(jsonString.utf8)) {
+            self = decoded
+        } else {
+            return nil
+        }
+    }
 }
