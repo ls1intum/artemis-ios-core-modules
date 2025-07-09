c3d8e4c97b5d741bbb43123a2f7ba8adc121d068
Anian Schleyer <98647423+anian03@users.noreply.github.com>
2025-07-09 22:02:29 +0200

    Fix exercise notification path

--- Sources/PushNotifications/Models/NavigatableNotification.swift
+++ Sources/PushNotifications/Models/NavigatableNotification.swift
@@ -24,7 +24,7 @@ extension NavigatableNotification {
 
     func exercisePath(courseId: Int?, exerciseId: Int?) -> String? {
         guard let courseId else { return nil }
-        var path = "courses/35/exercises"
+        var path = "courses/\(courseId)/exercises"
         guard let exerciseId else { return path }
         path += "/\(exerciseId)"
         return path
