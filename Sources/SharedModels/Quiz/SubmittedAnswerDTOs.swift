a14d665787204e9370495b8d421b9e44a9040c89
Anian Schleyer <anian.schleyer@tum.de>
2026-06-07 00:20:30 +0200

    Add helper inits for DTOs

---
+++ Sources/SharedModels/Quiz/SubmittedAnswerDTOs.swift
@@ -0,0 +1,27 @@
+//
+//  SubmittedAnswerDTOs.swift
+//  ArtemisCore
+//
+//  Created by Anian Schleyer on 01.06.26.
+//
+
+// Extensions for simpler initializers for omitting type information.
+// JSON Subtypes are seemingly not properly supported by OpenAPI generator
+
+public extension DTO.DragAndDropSubmittedAnswerFromLiveClient {
+    init(_ dto: Value2Payload) {
+        self.init(value1: .init(_type: "drag-and-drop"), value2: dto)
+    }
+}
+
+public extension DTO.ShortAnswerSubmittedAnswerFromLiveClient {
+    init(_ dto: Value2Payload) {
+        self.init(value1: .init(_type: "short-answer"), value2: dto)
+    }
+}
+
+public extension DTO.MultipleChoiceSubmittedAnswerFromLiveClient {
+    init(_ dto: Value2Payload) {
+        self.init(value1: .init(_type: "multiple-choice"), value2: dto)
+    }
+}
