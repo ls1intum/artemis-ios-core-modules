ec9a1d52ae467d600325d151a2cd29f3154518b7
Anian Schleyer <anian.schleyer@tum.de>
2026-08-04 15:33:18 +0200

    Add helper for participation

--- Sources/SharedModels/Quiz/StudentParticipation.swift
+++ Sources/SharedModels/Quiz/StudentParticipation.swift
@@ -1,8 +1,16 @@
 //
-//  File.swift
+//  StudentParticipation.swift
 //  ArtemisCore
 //
 //  Created by Anian Schleyer on 04.08.26.
 //
 
-import Foundation
+public extension DTO.StudentQuizParticipation {
+    var quizBatches: [DTO.QuizBatch] {
+        switch self {
+        case .afterQuizEnd(let withSolutions): withSolutions.exercise?.quizBatches ?? []
+        case .beforeQuizStart(let withoutQuestions): withoutQuestions.exercise?.quizBatches ?? []
+        case .liveQuiz(let withQuestions): withQuestions.exercise?.quizBatches ?? []
+        }
+    }
+}
