537f195e616b9b6e2eb677e896cf7161b3961716
Anian Schleyer <anian.schleyer@tum.de>
2026-09-17 01:01:48 +0200

    Add public init

--- Sources/SharedModels/Course/CourseAvailableTabsDTO.swift
+++ Sources/SharedModels/Course/CourseAvailableTabsDTO.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct CourseAvailableTabsDTO: Codable {
+public struct CourseAvailableTabsDTO: Codable, Hashable {
     public let lectures: Bool
     public let exams: Bool
     public let competencies: Bool
@@ -17,4 +17,24 @@ public struct CourseAvailableTabsDTO: Codable {
     public let learningPaths: Bool
     public let communication: Bool
     public let training: Bool
+
+    public init(lectures: Bool = true,
+                exams: Bool = true,
+                competencies: Bool = true,
+                tutorialGroups: Bool = true,
+                iris: Bool = true,
+                faq: Bool = true,
+                learningPaths: Bool = true,
+                communication: Bool = true,
+                training: Bool = true) {
+        self.lectures = lectures
+        self.exams = exams
+        self.competencies = competencies
+        self.tutorialGroups = tutorialGroups
+        self.iris = iris
+        self.faq = faq
+        self.learningPaths = learningPaths
+        self.communication = communication
+        self.training = training
+    }
 }
